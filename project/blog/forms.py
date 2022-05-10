0f540dc35022d5103ff9350071ee34ce5a9ea542
KimChanJin97 <cjkimhello97@gmail.com>
2022-05-10 19:58:48 +0900

    form, paginator 과제

--- project/blog/forms.py
+++ project/blog/forms.py
@@ -1,9 +1,12 @@
-##
 from django import forms
-from .models import Comment
+from .models import Comment, Blog
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model=Comment
         fields=('author_name', 'comment_text')
-##
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model=Blog
+        fields=('title', 'sub_title', 'contents', 'image')
--- project/blog/views.py
+++ project/blog/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Blog
-##
-from .forms import CommentForm
-##
+from .forms import CommentForm, BlogForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request): # 페이지 & 쿼리셋 가져오기 함수
     blogs = Blog.objects.all()
+    paginator = Paginator(blogs, 5) # blogs 데이터베이스를 8개로 페이지 구분
+    pagnum = request.GET.get('page') # 페이지별로 구분하고 그 변수page를 가져와서 pagnum변수에 저장
+    blogs = paginator.get_page(pagnum) # pagnum변수를 가져와서 blogs에 저장
     return render(request, 'home.html', {'blogs':blogs})
 
 def detail(request,id): # 페이지 & 쿼리셋 가져오기 함수
@@ -15,7 +17,14 @@ def detail(request,id): # 페이지 & 쿼리셋 가져오기 함수
     return render(request, 'detail.html', {'blog':blog, 'form':form})
 
 def new(request): # 페이지 가져오기 함수
-    return render(request, 'new.html')
+    if request.method == 'POST': # 데이터가 입력된 후 제출 버튼을 누르고 데이터 저장 = POST
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else: # 정보가 입력되지 않은 빈칸으로 되어있는 페이지 보여귀 = GET
+        form = BlogForm()
+        return render(request, 'new.html', {'form':form})
 
 def create(request): # DB 가져오는 함수
     new_blog = Blog()
